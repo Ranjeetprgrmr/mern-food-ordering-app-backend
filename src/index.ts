3126884ce78895fd99fa7d0dbf716cfb7f2427ed
unknown <9nadsg@gmail.com>
2024-09-29 17:53:50 +0545

    adding user profile feature

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,29 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoutes";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import { v2 as cloudinary }  from "cloudinary";
 
 dotenv.config();
 mongoose
  .connect(process.env.MONGODB_CONNECTION_STRING as string)
  .then(() => console.log('Connected to database!'));
 
+ 
+cloudinary.config({
+    cloud_name: 'ddhdofx50',
+    api_key: '764662927353585',
+    api_secret: '_SkiYtRKcFIVZn8WtSjF0qhL93U'
+})
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 app.get("/health", async(req: Request, res: Response ) => {
     res.send({ message: "health OK!" });
